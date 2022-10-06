f45efbeab3b8d744fd3f18189e3768d2b39e21ba
Todd Schultz <toddschultz@gmail.com>
2022-10-05 19:18:47 -0700

    changed names and deleted unused folder

--- baseBBQDatawTime/baseBBQDatawTime.ino
+++ baseBBQDatawTime/baseBBQDatawTime.ino
@@ -53,7 +53,6 @@
 
 #include <Arduino_MKRTHERM.h>
 #include <Arduino_MKRENV.h>
-// #include <ArduinoLowPower.h>
 #include "ThingSpeak.h"
 #include <WiFiNINA.h>
 #include <RTCZero.h>
@@ -61,28 +60,35 @@
 #include "secrets.h" 
 char ssid[] = SECRET_SSID;     // your network SSID (name)
 char password[] = SECRET_PWD;  // your network key
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
 
 // USER SETTINGS
 const int GMT = 0; //change this to adapt it to your time zone, Seattle -7
 const int dateOrder = 1;  // 1 = MDY; 0 for DMY
 // END USER SETTINGS
 
-WiFiClient  client;
-
-unsigned long myChannelNumber = SECRET_CH_ID;
-const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+WiFiClient client;
 
 RTCZero rtc; // create instance of real time clock
 int myhours, mins, secs, myday, mymonth, myyear;
 
 // Initialize values
 String myStatus = "";
-float limTempUp = 135; // Upper temperature range for smoker
+float limTempUp = 125; // Upper temperature range for smoker
 float limTempLow = 105; // Lower temperature range for smoker
-unsigned long time; // Time in milliseconds since program started
+
+// Sensor variables for running sum
+int ncounter = 0; // Loop counter
+float temperature       = 0;
+float humidity          = 0;
+float pressure          = 0;
+float smokertemperature = 0;
+float smokerreftemp     = 0;
 
 void setup() {
 
+  // Initialize WiFi    
   Serial.begin(115200);
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
@@ -120,15 +126,119 @@ void setup() {
   // wait 1 second for sensors to settle
   delay(1*1000);
 
-// Initialize real time clock
+  // Initialize real time clock
   rtc.begin();
   setRTC();  // get Epoch time from Internet Time Service
   fixTimeZone();
 }
 
 void loop() {
+  
+  // Reconnect to WiFi if needed
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, password); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
 
-  // Connect or reconnect to WiFi
+  // Get time 
+  secs = rtc.getSeconds();
+  mins = rtc.getMinutes();
+  // Fix time if needed
+  if (mins == 59 && secs == 0) setRTC();
+
+  // Get sample of data and add to running sum
+  ncounter = ncounter + 1;  //Increment counter
+  temperature = temperature + ENV.readTemperature();
+  humidity    = humidity + ENV.readHumidity();
+  pressure    = pressure + ENV.readPressure();
+  smokertemperature = smokertemperature + THERM.readTemperature();
+  smokerreftemp = smokerreftemp + THERM.readReferenceTemperature();
+
+  // Every 30 seconds average and write to ThingSpeak
+  if (secs == 0 || secs == 30) {
+    // Compute average, upload to ThingSpeak, and reset counter and sums
+    //Channel BBQed Data
+    // Fields Env pressure (kPa), Env RH (%), Env Temp (C), Smoker Temp (C)
+    ThingSpeak.setField(1, pressure/(float)ncounter);
+    ThingSpeak.setField(2, humidity/(float)ncounter);
+    ThingSpeak.setField(3, temperature/(float)ncounter);
+    ThingSpeak.setField(4, smokertemperature/(float)ncounter);
+
+    // Set status message based on smoker temperature
+    if (smokertemperature/(float)ncounter > limTempUp){
+      myStatus = String("Smoker running too hot!");
+    }
+    else if (smokertemperature/(float)ncounter < limTempLow){
+      myStatus = String("Smoker running too cold!");
+    }
+    else {
+      myStatus = String("Smoker temperature running normal.");
+    }
+    ThingSpeak.setStatus(myStatus);
+    
+    // write to the ThingSpeak channel
+    int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    if(x == 200){
+      Serial.println("Channel update successful.");
+    }
+    else{
+      Serial.println("Problem updating channel. HTTP error code " + String(x));
+    }  
+
+    // Display values
+    printDate();
+    printTime();
+
+    // print each of the sensor values
+    Serial.print("Env Temperature = ");
+    Serial.print(temperature/(float)ncounter);
+    Serial.println(" °C");
+
+    Serial.print("Env Humidity    = ");
+    Serial.print(humidity/(float)ncounter);
+    Serial.println(" %");
+
+    Serial.print("Env Pressure    = ");
+    Serial.print(pressure/(float)ncounter);
+    Serial.println(" kPa");
+  
+    Serial.print("Reference temperature = ");
+    Serial.print(smokerreftemp/(float)ncounter);
+    Serial.println(" °C");
+
+    Serial.print("Temperature = ");
+    Serial.print(smokertemperature/(float)ncounter);
+    Serial.println(" °C");
+    
+    Serial.print("Counter = ");
+    Serial.println(ncounter);
+    Serial.println(myStatus);    
+    Serial.println();
+    
+    // Reset counter and running sumsS
+    ncounter = 0;
+    temperature       = 0;
+    humidity          = 0;
+    pressure          = 0;
+    smokertemperature = 0;
+    smokerreftemp     = 0;
+  }
+
+  // Wait for new second
+  while (secs == rtc.getSeconds()) delay(10); // wait until seconds change
+}
+
+
+void setRTC() { // get the time from Internet Time Service
+  unsigned long epoch;
+  int numberOfTries = 0, maxTries = 6;
+  // Reconnect to WiFi if needed
   if(WiFi.status() != WL_CONNECTED){
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(SECRET_SSID);
@@ -140,116 +250,6 @@ void loop() {
     Serial.println("\nConnected.");
   }
   
-  // update date/time
-  //myyear = rtc.getYear();
-  //mymonth = rtc.getMonth();
-  //myday = rtc.getDay();
-  //myhours = rtc.getHours();
-  //mins = rtc.getMinutes();
-  //secs == rtc.getSeconds();
-  
-// Request seconds and deal with date/time
-  secs = rtc.getSeconds();
-  if (secs == 0) fixTimeZone(); // when secs is 0, update everything and correct for time zone
-  // otherwise everything else stays the same.
-  printDate();
-  printTime();
-  Serial.println();
-  if (secs == 15 || secs == 45) {
-    Serial.println("15 or 45 seconds!\n");
-  }
-  while (secs == rtc.getSeconds()) delay(10); // wait until seconds change
-  if (mins==59 && secs ==0) setRTC(); // get NTP time every hour at minute 59
-
-  // read all the sensor values
-  float temperature = ENV.readTemperature();
-  float humidity    = ENV.readHumidity();
-  float pressure    = ENV.readPressure();
-  float illuminance = ENV.readIlluminance();
-  float uva         = ENV.readUVA();
-  float uvb         = ENV.readUVB();
-  float uvIndex     = ENV.readUVIndex();
-
-  float smokertemperature = THERM.readTemperature();
-  float smokerreftemp = THERM.readReferenceTemperature();
-
-  // set the fields with the values
-  //Channel BBQed Data
-  // Fields Env pressure (kPa), Env RH (%), Env Temp (C), Smoker Temp (C)
-  ThingSpeak.setField(1, pressure);
-  ThingSpeak.setField(2, humidity);
-  ThingSpeak.setField(3, temperature);
-  ThingSpeak.setField(4, smokertemperature);
-  
-  // Set status message based on smoker temperature
-  if (smokertemperature > limTempUp){
-    myStatus = String("Smoker running too hot!");
-  }
-  else if (smokertemperature < limTempLow){
-    myStatus = String("Smoker running too cold!");
-  }
-  else {
-    myStatus = String("Smoker temperature running normal.");
-  }
-  ThingSpeak.setStatus(myStatus);
-
-  // write to the ThingSpeak channel
-  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-  if(x == 200){
-    Serial.println("Channel update successful.");
-  }
-  else{
-    Serial.println("Problem updating channel. HTTP error code " + String(x));
-  }  
-  
-  // print each of the sensor values
-  Serial.print("Env Temperature = ");
-  Serial.print(temperature);
-  Serial.println(" °C");
-
-  Serial.print("Env Humidity    = ");
-  Serial.print(humidity);
-  Serial.println(" %");
-
-  Serial.print("Env Pressure    = ");
-  Serial.print(pressure);
-  Serial.println(" kPa");
-
-  Serial.print("Env Illuminance = ");
-  Serial.print(illuminance);
-  Serial.println(" lx");
-
-  Serial.print("Env UVA         = ");
-  Serial.println(uva);
-
-  Serial.print("Env UVB         = ");
-  Serial.println(uvb);
-
-  Serial.print("UV Index    = ");
-  Serial.println(uvIndex);
-
-  // print an empty line
-  Serial.println();
-  
-  Serial.print("Reference temperature ");
-  Serial.print(smokerreftemp);
-  Serial.println(" °C");
-
-  Serial.print("Temperature ");
-  Serial.print(smokertemperature);
-  Serial.println(" °C");
-
-  // print an empty line
-  Serial.println();
-
-  // wait 30 second to print again
-  delay(30*1000);
-}
-
-
-void setRTC() { // get the time from Internet Time Service
-  unsigned long epoch;
-  int numberOfTries = 0, maxTries = 6;
   do {
     epoch = WiFi.getTime(); // The RTC is set to GMT or 0 Time Zone and stays at GMT.
     numberOfTries++;
